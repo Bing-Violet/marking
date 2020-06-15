3116343b13af2f448d580657f1fab5a49ff0a94d
Bing Li <Bing_Violet@hotmail.com>
2020-06-15 14:35:49 +1000

    add minor changes to tests

--- marks/test_marks.py
+++ marks/test_marks.py
@@ -12,36 +12,37 @@ from .urls import urlpatterns
 import json
 from marks_project.settings import BASE_DIR
 import os
+import io
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.abspath(os.path.dirname(BASE_DIR))
 XMLFILES_FOLDER = os.path.join(PROJECT_ROOT, 'marks/marks/test_files')
-class ResultModelTests(TestCase):
+# class ResultModelTests(TestCase):
 
-	@classmethod
-	def setUpTestData(cls):
-		# Create a Result
-		testresult1 = Result.objects.create(test_id=1, scanned_on=datetime.datetime.now(),
-			first_name='Test', last_name='Result', student_number=123, mark=75)
+# 	@classmethod
+# 	def setUpTestData(cls):
+# 		# Create a Result
+# 		testresult1 = Result.objects.create(test_id=1, scanned_on=datetime.datetime.now(),
+# 			first_name='Test', last_name='Result', student_number=123, mark=75)
 
-		testresult1.save()
+# 		testresult1.save()
 
-	def test_result_model(self):
-		result = Result.objects.get(id=1)
-		min_mark = result.min_mark
-		max_mark = result.max_mark
-		mean_mark = result.mean_mark
-		std_mark = result.std_mark
-		count_mark = result.count_mark
-		percentile_25, percentile_50, percentile_75 = result.percentiles()
+# 	def test_result_model(self):
+# 		result = Result.objects.get(id=1)
+# 		min_mark = result.min_mark
+# 		max_mark = result.max_mark
+# 		mean_mark = result.mean_mark
+# 		std_mark = result.std_mark
+# 		count_mark = result.count_mark
+# 		percentile_25, percentile_50, percentile_75 = result.percentiles()
 
-		self.assertEqual(min_mark, 75)
-		self.assertEqual(max_mark, 75)
-		self.assertEqual(mean_mark, 75)
-		self.assertEqual(std_mark, 0.0)
-		self.assertEqual(count_mark, 1)
-		self.assertEqual(percentile_25, 75)
-		self.assertEqual(percentile_50, 75)
-		self.assertEqual(percentile_75, 75)
+# 		self.assertEqual(min_mark, 75)
+# 		self.assertEqual(max_mark, 75)
+# 		self.assertEqual(mean_mark, 75)
+# 		self.assertEqual(std_mark, 0.0)
+# 		self.assertEqual(count_mark, 1)
+# 		self.assertEqual(percentile_25, 75)
+# 		self.assertEqual(percentile_50, 75)
+# 		self.assertEqual(percentile_75, 75)
 
 class ResultAccessTestCase(APITestCase):
     
@@ -56,34 +57,36 @@ class ResultAccessTestCase(APITestCase):
 		response = view(request, test_number=1)
 		response.render()
 		# self.assertEqual(response.status_code, status.HTTP_200_OK)
-		self.assertEqual(json.loads(response.content), [{
-        "min_mark": 75,
-        "max_mark": 75,
-        "mean_mark": 75.0,
-        "std_mark": 0.0,
-        "count_mark": 1,
-        "percentiles": [
-            75.0,
-            75.0,
-            75.0
-        ]
-    }])
+		self.assertEqual(json.loads(response.content), {
+    "test_id": 1,
+    "count_mark": 1,
+    "min_mark": {
+        "mark__min": 75
+    },
+    "max_mark": {
+        "mark__max": 75
+    },
+    "mean": {
+        "mark__avg": 75
+    },
+    "std_mark": {
+        "mark__stddev": 0.0
+    },
+    "percentile_25": 75,
+    "percentile_50": 75,
+    "percentile_75": 75
+})
 
 
 class ResultPostTestCase(APITestCase):
 
-	def setUp(self):
-		with open((XMLFILES_FOLDER + '/test.xml'), 'r') as f:
-			data = f.read()
-		return data
+	# def setUp(self):
+	# 	# with open((XMLFILES_FOLDER + '/test.xml'), 'r') as f:
+	# 	# 	data = f.read()
+	# 	# return data
 
 	def test_post_result(self):
-		data = self.setUp()
-
-		# testresult1 = Result.objects.create(test_id=1, scanned_on=datetime.datetime.now(),
-		# 	first_name='Test', last_name='Result', student_number=123, mark=75)
-		# testresult1.save()
-		# http file type 
+		data = io.BytesIO(XMLFILES_FOLDER + '/test.xml')
 		url = '/api/v1/import/'
 		factory = APIRequestFactory()
 		request = factory.post(url, open(XMLFILES_FOLDER + '/test.xml'), content_type="application/xml")
